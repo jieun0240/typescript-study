a411a8f125c8c2c6414e5b34e655886661530de5
jieun0240 <w2303@e-mirim.hs.kr>
2025-03-06 09:31:15 +0900

    연습ㅎ

---
+++ ts_add.ts
@@ -0,0 +1,8 @@
+function tsAdd(x: number, y: number): number {
+    return x + y;
+}
+
+console.log(tsAdd(10,20));
+// console.log(tsAdd(10));
+// console.log(tsAdd(10,20,30));
+// console.log(tsAdd("10","20"));
